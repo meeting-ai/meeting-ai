d1f1395bf81f526501b776457a8adf71a066fa86
Michael Beemer <michael.beemer@dynatrace.com>
2019-10-18 10:25:27 -0400

    Testing Slack response

--- src/slack.ts
+++ src/slack.ts
@@ -2,8 +2,9 @@ import { App } from "@slack/bolt";
 
 export const configure = (app: App) => {
   app.command("/meet", async ({ command, ack, say, context, payload }) => {
+    console.log(JSON.stringify(payload, null, 2));
     // Acknowledge command request
-    ack();
+    ack({ text: "test" });
     say({
       channel: payload.channel_id,
       text: `you said: ${command.text}`,
